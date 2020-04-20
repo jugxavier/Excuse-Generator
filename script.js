09303874fb154b887bef9feafe561196094e7097
Juliana Gonçalves <julianaxavier246@gmail.com>
2020-04-20 12:04:01 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,3 @@
+let quem = ["o cachorro", "meu vô", "sua tartaruga", "meu pássaro"];
+let oQue = ["coma", "irritado", "esmagado", "quebrado"];
+let quando = ["antes da aula", "bem na hora", "quando eu terminei", "durante o meu almoço", "enquanto eu estava orando"];
